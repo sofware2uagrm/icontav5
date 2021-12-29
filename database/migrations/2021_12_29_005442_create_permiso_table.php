913acbee5b57ec6778be4bdb159f0056609fdf6d
sofware2uagrm <sistemasuagrmleonel@gmail.com>
2021-12-28 20:56:25 -0400

    cree la migracion  permiso

---
+++ database/migrations/2021_12_29_005442_create_permiso_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePermisoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('permiso', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('permiso');
+    }
+}
